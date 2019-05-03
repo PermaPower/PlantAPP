7ed6607911bc13609d87f04ad219c26757393834
David Power <info@permaculturepower.com.au>
2019-05-03 14:39:43 +1000

    Finished Colour Palette

--- PlantApp/ColorPalette.swift
+++ PlantApp/ColorPalette.swift
@@ -10,7 +10,9 @@ import UIKit
 
 extension UIColor {
     
-    static let navigationBarColor = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
-    static let navigationBarTextColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
-
+    static let cRedish = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+    static let cWhite = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static let cdarkBlue = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+    static let cTeal = UIColor(red: 28/255, green: 164/255, blue: 182/255, alpha: 1)
+    static let clightBlue = UIColor(red: 218/255, green: 235/255, blue: 243/255, alpha: 1)
 }
--- PlantApp/ViewController.swift
+++ PlantApp/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellId = "CellID"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         setupNavigationStyle()
         
         setupNavigationButtons()
+        
+        setupTableView()
   
     }
     
@@ -27,14 +31,55 @@ class ViewController: UIViewController {
 
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.barTintColor = .navigationBarColor
-        navigationController?.navigationBar.tintColor = .navigationBarTextColor
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.navigationBarTextColor]
-        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.navigationBarTextColor]
+        navigationController?.navigationBar.barTintColor = .cRedish
+        navigationController?.navigationBar.tintColor = .cWhite
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.cWhite]
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.cWhite]
     }
     
     func setupNavigationButtons() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddPlantsButton))
+    }
+    
+    func setupTableView() {
+        
+        // tableView.separatorStyle = .none
+        tableView.separatorColor = .cWhite
+        tableView.backgroundColor = .cdarkBlue
+        
+        // Register tableViewCells
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        // Change the tableFooter to a blank view to hide row lines
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = "Plant Name"
+        
+        cell.textLabel?.textColor = .cWhite
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        cell.backgroundColor = .cTeal
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .clightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
     }
     
     @objc func handleAddPlantsButton() {
